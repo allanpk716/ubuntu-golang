c75a9a5d8237a1827ea6cb3abedd1db97b2e35ea
SuperNG6 <sleele@gmail.com>
2021-07-23 16:50:24 +0800

    armv7版构建失败，暂时先不搞

--- install.sh
+++ install.sh
@@ -7,13 +7,14 @@ if [[ ${ARCH} == "x86_64" ]]; then
     ARCH="amd64"
 elif [[ ${ARCH} == "aarch64" ]]; then
     ARCH="arm64"
-elif [[ ${ARCH} == "armv7l" ]]; then
-    ARCH="armv6l"
+# armv7版构建失败，暂时先不搞
+# elif [[ ${ARCH} == "armv7l" ]]; then
+#     ARCH="armv6l"
 else
     echo -e "${ERROR} This architecture is not supported."
     exit 1
 fi
-
+https://golang.org/dl/go1.15.14.linux-armv6l.tar.gz
 # download go
 # wget -c https://mirrors.ustc.edu.cn/golang/go${GO_VER}.linux-${ARCH}.tar.gz -O - | tar -xz -C /usr/local
 wget -c https://golang.org/dl/go${GO_VER}.linux-${ARCH}.tar.gz -O - | tar -xz -C /usr/local
